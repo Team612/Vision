7d0b17e3b333900f4ea792ab508bc9c01624bfbc
myrrhkhan <57461851+myrrhkhan@users.noreply.github.com>
2020-10-16 19:07:40 -0400

    set a file to configure user-specific variables.

--- screenrecorder.py
+++ screenrecorder.py
@@ -1,13 +1,15 @@
-import numpy as np, cv2, pyautogui
+import numpy as np, cv2, pyautogui, unique_vars
 
-screensize = pyautogui.size() #equals (2160, 1440)
+print(unique_vars.screensize)
 print("Press escape key to close window")
+img = pyautogui.screenshot(region = unique_vars.desired_pixels) 
+frame = np.array(img)
+frame = frame[...,::-1]
 
-while True: #Creates a screenshot to save as array lines 7-8, 9 BGR to RGB, 10-12 show image until escape key is pressed
-    img = pyautogui.screenshot(region = (1080, 720, 1080, 720)) #top, left, width, height
-    frame = np.array(img)
-    frame = frame[...,::-1]
+while True: #Creates a screenshot to save as array lines 7-8, 9 BGR to RGB, 10-13 show images until escape key is pressed
     cv2.imshow("screenshot", frame)
-    if cv2.waitKey(15) & 255 == 27:
+    instruct = cv2.imread('viewer_instructions.png', 1)
+    cv2.imshow("instructions", instruct)
+    if cv2.waitKey(unique_vars.time) & 255 == 27:
         break
 cv2.destroyAllWindows()
