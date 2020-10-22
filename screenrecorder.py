41f219e41cdd8ee210a2e17d46431666bb34790b
Jacob N <jnelsonf16@gmail.com>
2020-10-22 18:31:35 -0400

    Error fix

--- screenrecorder.py
+++ screenrecorder.py
@@ -2,11 +2,11 @@ import numpy as np, cv2, pyautogui, unique_vars
 
 print(unique_vars.screensize)
 print("Press escape key to close window")
-img = pyautogui.screenshot(region = unique_vars.desired_pixels) 
-frame = np.array(img)
-frame = frame[...,::-1]
 
 while True: #Creates a screenshot to save as array lines 7-8, 9 BGR to RGB, 10-13 show images until escape key is pressed
+    img = pyautogui.screenshot(region = unique_vars.desired_pixels) 
+    frame = np.array(img)
+    frame = frame[...,::-1]
     cv2.imshow("screenshot", frame)
     instruct = cv2.imread('viewer_instructions.png', 1)
     cv2.imshow("instructions", instruct)
--- unique_vars.py
+++ unique_vars.py
@@ -1,6 +1,6 @@
 import pyautogui
 
 screensize = pyautogui.size()
-desired_pixels = (1080, 720, 1080, 720) #top, left, width, height
+desired_pixels = (1510, 770, 650, 670) #top, left, width, height
 #set these based off of your screen size. on my 2160x1440 computer, these pixels are for the bottom right corner.
 time = 15 #amount of seconds that the picture will show itself before retaking another picture
