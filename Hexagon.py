2cdd6c7420227dd58296096b475a7127b1ba401b
myrrhkhan <57461851+myrrhkhan@users.noreply.github.com>
2020-10-29 18:47:18 -0400

    updated code. added documentation to hexagon.py. we just need to narrow down the contours to track the hexagon.

--- Hexagon.py
+++ Hexagon.py
@@ -3,13 +3,15 @@ import numpy as np, cv2, pyautogui, unique_vars
 print(unique_vars.screensize)
 print("Press escape key to close window")
 
-while True: #Creates a screenshot to save as array lines 7-8, 9 BGR to RGB, 10-13 show images until escape key is pressed
+while True: #Creates repeated screenshots
     img = pyautogui.screenshot(region = unique_vars.desired_pixels) 
     frame = np.array(img)
     end = frame.copy()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     edged = cv2.Canny(frame, 10, 300)
     '''cv2.imshow("name", edged)'''
+    '''line 7-9 create repeated/screenshots (a video, essentially) and output them as numpy arrays to'''
+    '''convert to rgb in line 10'''
     kernel = np.ones((5,5))
     dil = cv2.dilate(edged, kernel, iterations=1)
     contours, hierarchy = cv2.findContours(dil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -20,6 +22,7 @@ while True: #Creates a screenshot to save as array lines 7-8, 9 BGR to RGB, 10-1
             cv2.drawContours(end, contours, -1, (255, 0, 255), 8)
             area = cv2.contourArea(each)
             print(area)
+    '''Above lines adjust image and if the contour is big enough, it'll show the image with the contours.'''
     cv2.imshow("name", end)
     if cv2.waitKey(unique_vars.time) & 255 == 27:
         break
