9aac47c916dbe75d88f0e62909d11efde0019c3b
myrrhkhan <57461851+myrrhkhan@users.noreply.github.com>
2020-10-29 19:09:28 -0400

    GitHub/git commit -a are cursed af

--- unique_vars.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-
-screensize = pyautogui.size()
-desired_pixels = (1510, 770, 650, 670) #top, left, width, height
-#set these based off of your screen size. on my 2160x1440 computer, these pixels are for the bottom right corner.
-time = 15 #amount of seconds that the picture will show itself before retaking another picture
