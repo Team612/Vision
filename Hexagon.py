a5f61edec3a3689a0ba7f1c7beaf906a80c826df
myrrhkhan <57461851+myrrhkhan@users.noreply.github.com>
2020-10-29 18:19:55 -0400

    updated code that can nwow track and create contours. we just need to ifind a way to adetect hexiggons ns correctterrectly and we're golden :)

--- Hexagon.py
+++ Hexagon.py
@@ -6,12 +6,21 @@ print("Press escape key to close window")
 while True: #Creates a screenshot to save as array lines 7-8, 9 BGR to RGB, 10-13 show images until escape key is pressed
     img = pyautogui.screenshot(region = unique_vars.desired_pixels) 
     frame = np.array(img)
+    end = frame.copy()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     edged = cv2.Canny(frame, 10, 300)
-    cv2.imshow("name", edged)
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    im2 = cv2.drawContours(edged, contours, -1, 20)
-    
+    '''cv2.imshow("name", edged)'''
+    kernel = np.ones((5,5))
+    dil = cv2.dilate(edged, kernel, iterations=1)
+    contours, hierarchy = cv2.findContours(dil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for each in contours:
+        approx = cv2.approxPolyDP(each, cv2.arcLength(each, True), True)
+        print(approx)
+        if(len(approx) > 1):
+            cv2.drawContours(end, contours, -1, (255, 0, 255), 8)
+            area = cv2.contourArea(each)
+            print(area)
+    cv2.imshow("name", end)
     if cv2.waitKey(unique_vars.time) & 255 == 27:
         break
 cv2.destroyAllWindows() #removes windows when loop breaks, i.e. when esc pressed
